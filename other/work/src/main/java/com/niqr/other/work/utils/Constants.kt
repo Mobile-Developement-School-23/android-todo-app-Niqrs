95ad67b657ba1610cf598ac48ffe04c2675efe17
Niqrs <niqr.dev@gmail.com>
2023-07-09 04:51:41 +0300

    Synchronization FIX, Add Foreground SynchronizationService

--- other/work/src/main/java/com/niqr/other/work/utils/Constants.kt
+++ other/work/src/main/java/com/niqr/other/work/utils/Constants.kt
@@ -10,3 +10,5 @@ internal const val PERIODIC_SYNCHRONIZATION_WORK = "periodic_synchronization_wor
 
 internal const val SYNCHRONIZATION_CHANNEL_ID = "synchronization_channel_id"
 internal const val SYNCHRONIZATION_CHANNEL = "synchronization_channel"
+
+internal const val SYNCHRONIZATION_SERVICE_ID = 11
